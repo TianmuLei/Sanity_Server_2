dec3c5e9f51dffb07d2a1efbfb82def5ea58fdec
jiaxin Cheng <jiaxinch@usc.edu>
2017-10-14 11:23:53 -0700

    add new

--- SanityServer/src/server/User.java
+++ SanityServer/src/server/User.java
@@ -1,8 +1,9 @@
 package server;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class User {
-	
-	
 	String username;
 	String email;
 	String password1;
@@ -13,5 +14,57 @@ public class User {
 		password1=psword;
 		password2=psword2;
 	}
-
+	
+	public User(JSONObject JSONMessage){
+		try{
+			username=JSONMessage.getString("username");
+			email=JSONMessage.getString("Email");
+			password1=JSONMessage.getString("password1");
+			password2=JSONMessage.getString("password2");
+		}catch(JSONException e){
+			System.out.println(e.getMessage());
+		}
+		
+	}
 }
+
+	
+	
+	/*private String username;
+	private String email;
+	private String password1;
+	private String password2;
+	private String func;
+	public User(){
+		
+	}
+	public void setFunc(String a){
+		this.func = a;
+	}
+	public void setUsername(String a){
+		this.username = a;
+	}
+	public void setEmail(String a){
+		this.email = a;
+	}
+	public void setPassword1(String a){
+		this.password1 = a;
+	}
+	public void setPassword2(String a){
+		this.password2 = a;
+	}
+	public String getUsername(){
+		return this.username;
+	}
+	public String getEmail(){
+		return this.email;
+	}
+	public String getFunc(){
+		return this.func;
+	}
+	public String getPwd1(){
+		return this.password1;
+	}
+	public String getPwd2(){
+		return this.password2;
+	}*/
