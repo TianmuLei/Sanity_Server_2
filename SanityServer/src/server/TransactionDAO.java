e99da75fd2c4c43edae22d59e4668cdd81d28a81
jiaxin Cheng <jiaxinch@usc.edu>
2017-10-15 17:52:23 -0700

    finish update category and budget after adding a transaction

--- SanityServer/src/server/TransactionDAO.java
+++ SanityServer/src/server/TransactionDAO.java
@@ -8,6 +8,8 @@ import java.sql.SQLException;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.mysql.jdbc.UpdatableResultSet;
+
 public class TransactionDAO extends DAO{
 	public JSONObject createTransaction(Transaction toAdd){	
 		try{
@@ -19,6 +21,7 @@ public class TransactionDAO extends DAO{
 			}
 			else{
 				addTransactionDB(toAdd);
+				updateDB(toAdd);
 				System.out.println("return success");
 				message.put("status", "success");
 			}	
@@ -70,6 +73,59 @@ public class TransactionDAO extends DAO{
 		Connection conn= getDBConnection();
 		PreparedStatement insert = conn.prepareStatement("INSERT INTO SanityDB.Transaction (Transaction_description, "
 				+ "User_id, Transaction_amount, Transaction_date,Budget_id,Category_id) VALUE (?,?,?,?,?,?)");
-		insert.
+		insert.setString(1, toAdd.description);
+		insert.setInt(2, toAdd.userID);
+		insert.setDouble(3, toAdd.amount);
+		insert.setDate(4, java.sql.Date.valueOf(toAdd.date));
+		insert.setInt(5, toAdd.budgetID);
+		insert.setInt(6, toAdd.categoryID);
+		try{
+			insert.executeUpdate();
+		}catch(SQLException e){
+			System.out.println("check transaction error ");
+		}finally{
+			if(insert!=null){
+				insert.close();
+			}
+			if(conn!=null){
+				conn.close();
+			}
+		}
+	}
+	private void updateDB(Transaction toAdd) throws SQLException{
+		Connection conn= getDBConnection();
+		Double budgetspent=0.0;
+		Double categoryspent=0.0;
+		PreparedStatement getbudget = conn.prepareStatement("SELECT * FROM SanityDB.Budget WHERE Budget_id=?");
+		PreparedStatement getcategory = conn.prepareStatement("SELECT* FROM SanityDB.Category WHERE Category_id=?");
+		getbudget.setInt(1, toAdd.budgetID);
+		getcategory.setInt(1, toAdd.categoryID);
+		try{
+			ResultSet budget =getbudget.executeQuery();
+			ResultSet cate = getcategory.executeQuery();
+			budget.next();
+			budgetspent=budget.getDouble("Budget_spent");
+			cate.next();
+			categoryspent=cate.getDouble("Category_spent");
+		}catch(SQLException e){
+			System.out.println("check transaction error ");
+		}finally{
+			if(getbudget!=null){
+				getbudget.close();
+			}
+			if(getcategory!=null){
+				getcategory.close();
+			}
+		}
+		budgetspent+= toAdd.amount;
+		categoryspent+=toAdd.amount;
+		PreparedStatement updateBudget= conn.prepareStatement("UPDATE SanityDB.Budget SET Budget_spent=?"
+				+ "WHERE Budget_id =? ");
+		PreparedStatement updateCategory= conn.prepareStatement("UPDATE SanityDB.Category SET Category_spent=?"
+				+ "WHERE Category_id =? ");
+		updateBudget.setDouble(1, budgetspent);
+		updateBudget.setDouble(2, toAdd.budgetID);
+		updateCategory.setDouble(1, categoryspent);
+		updateCategory.setDouble(2, toAdd.categoryID);
 	}
 }
