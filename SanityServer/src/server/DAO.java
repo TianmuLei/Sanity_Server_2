2eec764393149deaa667fe59bb5f70133c2e6eb5
jiaxin Cheng <jiaxinch@usc.edu>
2017-10-16 11:29:06 -0700

    debug

--- SanityServer/src/server/DAO.java
+++ SanityServer/src/server/DAO.java
@@ -194,7 +194,7 @@ public class DAO {
 				temp.put("budgetSpent", rs.getDouble("Budget_spent"));
 				temp.put("threshold",rs.getInt("Threshold"));
 				temp.put("frequency", rs.getInt("Frequency"));
-				temp.put("period", rs.getInt("Period"));
+				temp.put("period", rs.getInt("Budget_period"));
 				Jarray.put(temp);
 			}
 		}catch(JSONException e){
