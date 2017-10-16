dc9fc9819abdc99887656c1ad33a77c084297884
Tianmu Lei <tianmule@usc.edu>
2017-10-16 13:46:53 -0700

    debug date

--- SanityServer/src/server/DateCal.java
+++ SanityServer/src/server/DateCal.java
@@ -30,7 +30,7 @@ public class DateCal {
 		return sdf.format(startday.getTime());
 	}
 	public static String calculateCurrentStart(String startSate,Integer period,int history){
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MMM-dd");
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		Calendar today = Calendar.getInstance();
 		Calendar startday = Calendar.getInstance();
 		try {
@@ -39,18 +39,39 @@ public class DateCal {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 		today.set(Calendar.HOUR_OF_DAY, 0);
 		startday.set(Calendar.HOUR_OF_DAY, 0);
 		
-		while(startday.compareTo(today)<=0){
-			startday.add(Calendar.DAY_OF_MONTH, period);	
+		if(startday.compareTo(today)<=0){
+			
+			System.out.println("here");
+			
+			while(startday.compareTo(today)<=0){
+				startday.add(Calendar.DAY_OF_MONTH, period);	
+				//System.out.println("add");
+				//System.out.println(sdf.format(startday.getTime()));
+				//System.out.println(sdf.format(today.getTime()));
+			}
+			//this is the current start day 
+			startday.add(Calendar.DAY_OF_MONTH, -period);
+			
+			
 		}
-		//this is the current start day 
-		startday.add(Calendar.DAY_OF_MONTH, -period);
+		else{
+			
+			while(startday.compareTo(today)>0){
+				startday.add(Calendar.DAY_OF_MONTH, -period);	
+			}
+			
+			
+		}
+		
+		
 		startday.add(Calendar.DAY_OF_MONTH, -history*period);
 		return sdf.format(startday.getTime());
 		
 	}
+	
+	
 
 }
