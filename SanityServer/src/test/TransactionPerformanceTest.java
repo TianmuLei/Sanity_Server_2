3cc7a1ecf7c9f4b927caf49e0a17ad58a43abd23
Yang Chen <chen716@usc.edu>
2017-11-19 21:37:14 -0800

    added transaction performance test

--- SanityServer/src/test/TransactionPerformanceTest.java
+++ SanityServer/src/test/TransactionPerformanceTest.java
@@ -3,8 +3,6 @@ package test;
 import static org.junit.Assert.*;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.junit.After;
@@ -15,51 +13,51 @@ import server.DAO;
 import server.DateCal;
 import server.Transaction;
 import server.TransactionDAO;
-import server.User;
 
-public class DAOPerformanceTest {
+public class TransactionPerformanceTest {
 	TransactionDAO transactionDAO = new TransactionDAO();
-	DAO tests = new DAO();
 	Connection conn;
+	
+	
 	@Before
 	public void setUp() throws Exception {
-		
 		DAO.testing = 0;
 		conn =transactionDAO.getDBConnection();
-		String today =DateCal.today();
-		String description = "this is a testing transaction";
-		Transaction toAdd = new Transaction("yang@usc.edu", description, "qqq","testing",today,3.3);
-		
-		
-			transactionDAO.createTransaction(toAdd);
-		
 	}
-
 	@After
 	public void tearDown() throws Exception {
 	}
 
 	@Test
-	public void testGetEverythingOld() {
+	public void testCheckTransactionExistNew() {
+		String today =DateCal.today();
+		String description = "this is a testing transaction";
+		Transaction toAdd = new Transaction("yang@usc.edu", description, "qqq","testing",today,3.3);
 		long startTime = System.currentTimeMillis();
-		User toadd = new User("yang", "yang@usc.edu", "123", "456");
-		for(int i = 0; i < 100; i ++ ){
-			//tests.getEverythingOld(toadd, 0);
-			tests.fetchAllData("chen716@usc.edu", 0);
+		for(int i = 0 ; i < 100; i ++ ){
+			try {
+				transactionDAO.checkTransactionExist(toAdd);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		long endTime = System.currentTimeMillis();
 		System.out.println("First Time: "+ (endTime - startTime));
 		long time1 = endTime - startTime;
 		startTime = System.currentTimeMillis();
-		
-		for(int i = 0; i < 100; i ++ ){
-			tests.getEverythingOld(toadd, 0);
-			//tests.fetchAllData("chen716@usc.edu", 0);
+		for(int i = 0 ; i < 100; i ++ ){
+			try {
+				transactionDAO.checkTransactionExistNew(toAdd);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		endTime = System.currentTimeMillis();
+		System.out.println("First Time: "+ (endTime - startTime));
 		long time2 = endTime - startTime;
-		System.out.println("Second Time: "+(endTime - startTime));
-		assertEquals(true, time1<time2);
+		assertEquals(true, time1>time2);
 	}
 
 }
